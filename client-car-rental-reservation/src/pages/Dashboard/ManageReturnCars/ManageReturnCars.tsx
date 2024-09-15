66d856e12ea828ee22276a015f7b23640d0742dc
mhrafi21 <12mhrafi@gmail.com>
2024-09-15 23:05:23 +0600

    update: some functionality bugs

--- client-car-rental-reservation/src/pages/Dashboard/ManageReturnCars/ManageReturnCars.tsx
+++ client-car-rental-reservation/src/pages/Dashboard/ManageReturnCars/ManageReturnCars.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
-import { useCarReturnAndUpdateDateMutation, useGetAllBookingsQuery } from "../../../redux/baseApi";
+import {
+  useCarReturnAndUpdateDateMutation,
+  useGetAllBookingsQuery,
+} from "../../../redux/baseApi";
 import { TBooking } from "../../../interfaces";
 
 const ManageReturnCars: React.FC = () => {
@@ -8,34 +11,17 @@ const ManageReturnCars: React.FC = () => {
   const [editBookingId, setEditBookingId] = useState<string | null>(null);
   const [newEndTime, setNewEndTime] = useState<string>("");
 
-  const handleEditClick = async(id: string, currentEndTime: string) => {
+  const handleEditClick = async (id: string, currentEndTime: string) => {
     setEditBookingId(id);
     setNewEndTime(currentEndTime);
-
-    const updateTime = {
-      bookingId: id,
-      endTime: newEndTime,
-    }
-
-    console.log(updateTime);
-
-    const res = await updateReturnCar(updateTime).unwrap();
-    console.log(res);
-
-
   };
 
-  const handleUpdateEndTime = async(id: string) => {
+  const handleUpdateEndTime = async (id: string) => {
     const updateTime = {
       bookingId: id,
       endTime: newEndTime,
-    }
-
-    console.log(updateTime);
-
-    const res = await updateReturnCar(updateTime).unwrap();
-    console.log(res);
-
+    };
+    await updateReturnCar(updateTime).unwrap();
     setEditBookingId(null);
     setNewEndTime("");
   };
@@ -44,7 +30,9 @@ const ManageReturnCars: React.FC = () => {
     <div className="p-6 bg-gray-100 w-full">
       <div className="max-w-6xl mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
         <div className="px-6 py-4 border-b border-gray-200">
-          <h1 className="text-2xl font-semibold text-gray-800">Manage Bookings</h1>
+          <h1 className="text-2xl font-semibold text-gray-800">
+            Manage Bookings
+          </h1>
         </div>
         <div className="overflow-x-auto">
           <table className="min-w-full divide-y divide-gray-200">
@@ -74,71 +62,79 @@ const ManageReturnCars: React.FC = () => {
               </tr>
             </thead>
             <tbody className="bg-white divide-y divide-gray-200">
-            {isLoading && <div>Loading...</div>}
-              { data?.data && data?.data?.map((booking: TBooking) => (
-                <tr key={booking._id}>
-                  <td className="py-4 px-6 text-sm font-medium text-gray-900">
-                    {booking?.user?.name}
-                  </td>
-                  <td className="py-4 px-6 text-sm text-gray-500">
-                    {booking?.car?.name}
-                  </td>
-                  <td className="py-4 px-6 text-sm text-gray-500">
-                    {booking?.date} ({booking?.startTime})
-                  </td>
-                  <td className="py-4 px-6 text-sm text-gray-500">
-                    {editBookingId === booking._id ? (
-                      <input
-                        type="time"
-                        value={newEndTime}
-                        onChange={(e) => setNewEndTime(e.target.value)}
-                        className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                      />
-                    ) : (
-                      booking?.endTime
-                    )}
-                  </td>
-                  <td className="py-4 px-6 text-sm text-gray-500">
-                    {booking?.totalCost}
-                  </td>
-                  <td className="py-4 px-6 text-sm text-gray-500">
-                    <p
-                      className={`inline-flex px-2 py-1 font-semibold leading-5 rounded-full text-red-600`}
-                    >
-                      {booking?.approved === true ? (
-                        <span className="text-green-600">Approved</span>
+              {isLoading && <div>Loading...</div>}
+              {data?.data &&
+                data?.data?.map((booking: TBooking) => (
+                  <tr key={booking._id}>
+                    <td className="py-4 px-6 text-sm font-medium text-gray-900">
+                      {booking?.user?.name}
+                    </td>
+                    <td className="py-4 px-6 text-sm text-gray-500">
+                      {booking?.car?.name}
+                    </td>
+                    <td className="py-4 px-6 text-sm text-gray-500">
+                      {booking?.date} ({booking?.startTime})
+                    </td>
+                    <td className="py-4 px-6 text-sm text-gray-500">
+                      {editBookingId === booking._id ? (
+                        <input
+                          type="time"
+                          value={newEndTime}
+                          onChange={(e) => setNewEndTime(e.target.value)}
+                          className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                        />
                       ) : (
-                        <span className="text-red-600">Pending</span>
+                        booking?.endTime
                       )}
-                    </p>
-                  </td>
-                  <td className="py-4 px-6 text-sm flex gap-2">
-                    {editBookingId === booking._id ? (
-                      <>
-                        <button
-                          onClick={() => handleUpdateEndTime(booking._id as string)}
-                          className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow hover:bg-blue-600 transition duration-300"
-                        >
-                          Save
-                        </button>
-                        <button
-                          onClick={() => setEditBookingId(null)}
-                          className="bg-gray-500 text-white px-4 py-2 rounded-lg shadow hover:bg-gray-600 transition duration-300"
-                        >
-                          Cancel
-                        </button>
-                      </>
-                    ) : (
-                      <button
-                        onClick={() => handleEditClick(booking._id as string, booking?.endTime)}
-                        className="bg-yellow-500 text-white px-4 py-2 rounded-lg shadow hover:bg-yellow-600 transition duration-300"
+                    </td>
+                    <td className="py-4 px-6 text-sm text-gray-500">
+                      $ {booking?.totalCost}
+                    </td>
+                    <td className="py-4 px-6 text-sm text-gray-500">
+                      <p
+                        className={`inline-flex px-2 py-1 font-semibold leading-5 rounded-full text-red-600`}
                       >
-                        Edit
-                      </button>
-                    )}
-                  </td>
-                </tr>
-              ))}
+                        {booking?.approved === true ? (
+                          <span className="text-green-600">Approved</span>
+                        ) : (
+                          <span className="text-red-600">Pending</span>
+                        )}
+                      </p>
+                    </td>
+                    <td className="py-4 px-6 text-sm flex gap-2">
+                      {editBookingId === booking._id ? (
+                        <>
+                          <button
+                            onClick={() =>
+                              handleUpdateEndTime(booking._id as string)
+                            }
+                            className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow hover:bg-blue-600 transition duration-300"
+                          >
+                            Save
+                          </button>
+                          <button
+                            onClick={() => setEditBookingId(null)}
+                            className="bg-gray-500 text-white px-4 py-2 rounded-lg shadow hover:bg-gray-600 transition duration-300"
+                          >
+                            Cancel
+                          </button>
+                        </>
+                      ) : (
+                        <button
+                          onClick={() =>
+                            handleEditClick(
+                              booking._id as string,
+                              booking?.endTime
+                            )
+                          }
+                          className="bg-yellow-500 text-white px-4 py-2 rounded-lg shadow hover:bg-yellow-600 transition duration-300"
+                        >
+                          Edit
+                        </button>
+                      )}
+                    </td>
+                  </tr>
+                ))}
             </tbody>
           </table>
         </div>
--- client-car-rental-reservation/src/pages/Dashboard/ManageUserBooking/ManageUserBooking.tsx
+++ client-car-rental-reservation/src/pages/Dashboard/ManageUserBooking/ManageUserBooking.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   useCarBookingCancelMutation,
   useGetSingleBookingsQuery,
@@ -8,10 +8,10 @@ import BookingCard from "./BookingCard";
 import Swal from "sweetalert2";
 
 const BookingManagement: React.FC = () => {
-  // const [bookings, setBookings] = useState<Booking[]>([]);
-
   const { data, isLoading } = useGetSingleBookingsQuery(undefined);
   const [addToModify] = useCarBookingCancelMutation(undefined);
+  
+  const [activeTab, setActiveTab] = useState<"upcoming" | "past">("upcoming"); // Tab state
 
   const handleModify = async (id: string) => {
     try {
@@ -22,7 +22,7 @@ const BookingManagement: React.FC = () => {
         showCancelButton: true,
         confirmButtonColor: "#3085d6",
         cancelButtonColor: "#d33",
-        confirmButtonText: "Yes, Booked it!",
+        confirmButtonText: "Yes, Book it!",
       });
 
       if (result.isConfirmed) {
@@ -41,7 +41,7 @@ const BookingManagement: React.FC = () => {
         "There was a problem booking your product.",
         "error"
       );
-      console.error("Failed to Cancel product:", error);
+      console.error("Failed to book product:", error);
     }
   };
 
@@ -54,7 +54,7 @@ const BookingManagement: React.FC = () => {
         showCancelButton: true,
         confirmButtonColor: "#3085d6",
         cancelButtonColor: "#d33",
-        confirmButtonText: "Yes, Cancelled it!",
+        confirmButtonText: "Yes, Cancel it!",
       });
 
       if (result.isConfirmed) {
@@ -64,57 +64,86 @@ const BookingManagement: React.FC = () => {
         }).unwrap();
 
         if (res?.success === true) {
-          Swal.fire("Booked!", `${res?.message}`, "success");
+          Swal.fire("Cancelled!", `${res?.message}`, "success");
         }
       }
     } catch (error) {
       Swal.fire(
         "Failed!",
-        "There was a problem Cancel your product.",
+        "There was a problem cancelling your product.",
         "error"
       );
-      console.error("Failed to Cancel product:", error);
+      console.error("Failed to cancel product:", error);
     }
   };
 
-  // const upcomingBookings = bookings.filter(booking => new Date(booking.date) >= new Date());
-  // const pastBookings = bookings.filter(booking => new Date(booking.date) < new Date());
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  // Filter upcoming and past bookings based on current date
+
+  const upcomingBookings = data?.data.filter((booking: TBookingState) => booking?.approved === true);
+  const pastBookings = data?.data.filter((booking: TBookingState) => booking?.totalCost !== 0);
 
   return (
     <div className="container mx-auto p-8">
       <h1 className="text-3xl font-bold mb-6">Booking Management</h1>
-      {isLoading && <div>Loading...</div>}
-      <div className="mb-8">
-        <h2 className="text-2xl font-semibold mb-4">Upcoming Bookings</h2>
-        {data?.data.length > 0 ? (
-          data?.data.map((booking: TBookingState) => (
-            <BookingCard
-              key={booking._id}
-              booking={booking}
-              onModify={handleModify}
-              onCancel={handleCancel}
-            />
-          ))
-        ) : (
-          <p>No upcoming bookings.</p>
-        )}
+      
+      {/* Tabs for switching between upcoming and past bookings */}
+      <div className="flex mb-6">
+        <button
+          className={`px-4 py-2 mr-2 ${
+            activeTab === "upcoming" ? "bg-blue-500 text-white" : "bg-gray-200"
+          } rounded-md`}
+          onClick={() => setActiveTab("upcoming")}
+        >
+          Upcoming Bookings
+        </button>
+        <button
+          className={`px-4 py-2 ${
+            activeTab === "past" ? "bg-blue-500 text-white" : "bg-gray-200"
+          } rounded-md`}
+          onClick={() => setActiveTab("past")}
+        >
+          Past Bookings
+        </button>
       </div>
 
-      <div>
-        <h2 className="text-2xl font-semibold mb-4">Past Bookings</h2>
-        {data?.data.length > 0 ? (
-          data?.data.map((booking: TBookingState) => (
-            <BookingCard
-              key={booking._id}
-              booking={booking}
-              onModify={handleModify}
-              onCancel={handleCancel}
-            />
-          ))
-        ) : (
-          <p>No past bookings.</p>
-        )}
-      </div>
+      {/* Conditional rendering for Upcoming and Past bookings */}
+      {activeTab === "upcoming" ? (
+        <div>
+          <h2 className="text-2xl font-semibold mb-4">Upcoming Bookings</h2>
+          {upcomingBookings?.length > 0 ? (
+            upcomingBookings.map((booking: TBookingState) => (
+              <BookingCard
+                key={booking._id}
+                booking={booking}
+                onModify={handleModify}
+                onCancel={handleCancel}
+              />
+            ))
+          ) : (
+            <p>No upcoming bookings.</p>
+          )}
+        </div>
+      ) : (
+        <div>
+          <h2 className="text-2xl font-semibold mb-4">Past Bookings</h2>
+          {pastBookings?.length > 0 ? (
+            pastBookings.map((booking: TBookingState) => (
+              <BookingCard
+                key={booking._id}
+                booking={booking}
+                onModify={handleModify}
+                onCancel={handleCancel}
+              />
+            ))
+          ) : (
+            <p>No past bookings.</p>
+          )}
+        </div>
+      )}
     </div>
   );
 };
